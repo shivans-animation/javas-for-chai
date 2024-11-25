98a138b3a7f400f09e0c06064ff5cf4d7c517d64
Shivanshu yadav <shivans7290@gmail.com>
2024-11-25 08:03:11 +0000

    control config

--- basics/text.js
+++ basics/text.js
@@ -1,1 +1,9 @@
-console.log("shivanshu")
+const accountid = 2881188
+let accountmail = "shiabsu@gmail.com"
+var accountpassword = "350650"
+accountcity = "jaipur"
+
+console.table([accountid, accountmail, accountpassword, accountcity])
+/*
+class 04
+/*
